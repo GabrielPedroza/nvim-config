629cbaf5e3dfe6ccadb8c8b1fc66447c225bc73a
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-08-21 16:17:39 -0400

    new colortheme

--- init.lua
+++ init.lua
@@ -805,21 +805,26 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+  -- Colortheme
+  {
+    'sho-87/kanagawa-paper.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('kanagawa-paper').setup {
+        undercurl = true,
+        transparent = true,
+        gutter = false,
+        dimInactive = true,
+        terminalColors = true,
+        commentStyle = { italic = true },
+        functionStyle = { italic = false },
+        keywordStyle = { italic = false, bold = false },
+        statementStyle = { italic = false, bold = false },
+        typeStyle = { italic = false },
+        colors = { theme = {}, palette = {} },
+      }
+      require('kanagawa-paper').load()
     end,
   },
 
